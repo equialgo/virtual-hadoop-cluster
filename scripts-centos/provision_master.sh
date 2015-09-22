a3719d568fc19fd00361b05ba740338108b3f61e
equialgo <equialgo@gmail.com>
2015-09-22 23:51:14 +0200

    adding extra dsn adressess to make cloudera install go smoother

--- scripts-centos/provision_master.sh
+++ scripts-centos/provision_master.sh
@@ -39,3 +39,8 @@ echo vagrant | passwd vagrant --stdin
 /etc/init.d/ntpd start
 chkconfig --add ntpd
 chkconfig ntpd on
+
+# add dns addresses
+echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
+
--- scripts-centos/provision_worker.sh
+++ scripts-centos/provision_worker.sh
@@ -12,3 +12,7 @@ echo vagrant | passwd vagrant --stdin
 /etc/init.d/ntpd start
 chkconfig --add ntpd
 chkconfig ntpd on
+
+# add dns addresses
+echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
